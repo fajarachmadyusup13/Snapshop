807bad50218d4448350f841af367f259ebf976b8
farhandzakyarvianto <farhandzaky25@gmail.com>
2017-04-29 14:13:19 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Snapshop\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'barcode',
+        'product_name',
+        'description',
+        'price',
+        'stock',
+        'brand_id',
+        'category_id',
+    ];
+
+    /**
+     * Get the brand that the product belongs to.
+     */
+    public function brand()
+    {
+        return $this->belongsTo('Snapshop\Models\Brand','brand_id');
+    }
+
+    /**
+     * Get the category that the product belongs to.
+     */
+    public function category()
+    {
+        return $this->belongsTo('Snapshop\Models\Category','category_id');
+    }
+}
